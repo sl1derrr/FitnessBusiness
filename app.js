9b7445814e37dd2925705e5ca1abc337e7255d37
sl1derrr <maksmakarov331@gmail.com>
2025-03-27 23:13:28 +0300

    Bug Fix

--- app.js
+++ app.js
@@ -65,4 +65,62 @@ function setupApp() {
 // Запускаем приложение при загрузке страницы
 document.addEventListener('DOMContentLoaded', initApp);
 
+const list = document.querySelectorAll('.list');
+const contents = document.querySelectorAll('.content');
+
+/* Лоудер */ 
+document.addEventListener("DOMContentLoaded", function() { /* При загрузке*/
+
+    setTimeout(function() {
+        loader.classList.add("hidden");
+
+        setTimeout(function() { /* Скрытие лоудера, появление контента */
+            loader.style.display = "none";
+            main.style.opacity = "1";
+            main.style.visibility = "visible";
+            main.home.page.visibility = "visible"
+        }, 500); // Завершение анимации
+    }, 2000);
+});
+
+
+// Функционал активного элемента меню
+
+function activeLink() { 
+    list.forEach(item => item.classList.remove('active'));
+    this.classList.add('active');
+
+    const contentId = this.querySelector('a').getAttribute('data-content');
+
+    contents.forEach(content => content.classList.remove('show'));
+    document.getElementById(contentId).classList.add('show');
+}
+
+list.forEach(item => item.addEventListener('click', activeLink));
+
+// Спарк
+
+document.body.addEventListener("click", (e) => {
+    const sparkCount = 12; // Количество частиц
+    for (let i = 0; i < sparkCount; i++) {
+      const spark = document.createElement("div");
+      spark.className = "spark";
+      document.body.appendChild(spark);
+  
+      // Устанавливаем положение и направление
+      const angle = (i * 360) / sparkCount;
+      const dx = 50 * Math.cos((angle * Math.PI) / 180); // Смещение по X
+      const dy = 50 * Math.sin((angle * Math.PI) / 180); // Смещение по Y
+      spark.style.setProperty("--dx", `${dx}px`);
+      spark.style.setProperty("--dy", `${dy}px`);
+  
+      // Устанавливаем начальные координаты частиц
+      spark.style.left = `${e.pageX}px`;
+      spark.style.top = `${e.pageY}px`;
+  
+      // Удаление частиц после завершения анимации
+      spark.addEventListener("animationend", () => spark.remove());
+    }
+  });
+
 
